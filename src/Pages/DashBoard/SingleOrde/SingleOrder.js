9a786e4954735b22bbc1c40daec5a3f11d877dc9
euhansarkar <euhansarkar@gmail.com>
2022-11-28 01:22:31 +0600

    order dedetation added

--- src/Pages/DashBoard/SingleOrde/SingleOrder.js
+++ src/Pages/DashBoard/SingleOrde/SingleOrder.js
@@ -1,18 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { primary } from "daisyui/src/colors";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SingleOrder = ({ order, index }) => {
+const SingleOrder = ({ order, index, setDeleteOrder }) => {
   const { _id, product_id } = order;
 
   const { data: product = [] } = useQuery({
     queryKey: [`order`, product_id],
-    queryFn: async() => {
+    queryFn: async () => {
       const res = await fetch(`http://localhost:5000/products/${product_id}`);
       const data = await res.json();
       return data;
-    }
+    },
   });
 
   const { title, resalePrice, isPaid } = product;
@@ -31,7 +30,13 @@ const SingleOrder = ({ order, index }) => {
         )}
       </td>
       <td>
-        <button className="btn btn-error btn-xs">delete</button>
+        <label
+          onClick={() => setDeleteOrder(order)}
+          htmlFor="ConfirmationModal"
+          className="btn btn-xs btn-error"
+        >
+          delete
+        </label>
       </td>
     </tr>
   );
--- src/Pages/MyOrders/MyOrders.js
+++ src/Pages/MyOrders/MyOrders.js
@@ -1,20 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import toast from "react-hot-toast";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 import SingleOrder from "../DashBoard/SingleOrde/SingleOrder";
+import ConfirmationModal from "../Shared/ConfirmationModal/ConfirmationModal";
 
 const MyOrders = () => {
   const {
     user: { email },
   } = useContext(AuthContext);
+  const [deleteOrder, setDeleteOrder] = useState(null);
 
-  const { data: orders, isLoading } = useQuery({
+  const { data: orders, isLoading, refetch } = useQuery({
     queryKey: [`orders`, email],
     queryFn: async () => {
       const res = await fetch(`http://localhost:5000/orders?email=${email}`, {
         headers: {
-            authorization: `bearer ${localStorage.getItem(`accessToken`)}`
-        }
+          authorization: `bearer ${localStorage.getItem(`accessToken`)}`,
+        },
       });
       const data = await res.json();
       return data;
@@ -25,30 +28,67 @@ const MyOrders = () => {
     return <p>loading</p>;
   }
 
+  const closeModal = () => {
+    setDeleteOrder(null);
+  };
+
+
+  const handleDeleteOrder = (order) => {
+    fetch(`http://localhost:5000/orders/${order._id}`, {
+      method: `DELETE`,
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.deletedCount > 0 ){
+        toast.success(`product ${order.title} deleted successfully`)
+        refetch();
+      }
+    })
+  };
+    
 
   return (
-    <div>
-      <h2 className="my-5 font-semibold text-4xl text-center">my orders</h2>
-      <div className="overflow-x-auto">
-        <div className="overflow-x-auto w-full">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>Product Name</th>
-                <th>Product Price</th>
-                <th>Product Quantity</th>
-                <th>Payment Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order, index) => <SingleOrder order={order} key={order._id} index={index}></SingleOrder>)}
-            </tbody>
-          </table>
+    <>
+      <div>
+        <h2 className="my-5 font-semibold text-4xl text-center">my orders</h2>
+        <div className="overflow-x-auto">
+          <div className="overflow-x-auto w-full">
+            <table className="table w-full">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Product Name</th>
+                  <th>Product Price</th>
+                  <th>Product Quantity</th>
+                  <th>Payment Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order, index) => (
+                  <SingleOrder
+                    setDeleteOrder={setDeleteOrder}
+                    order={order}
+                    key={order._id}
+                    index={index}
+                  ></SingleOrder>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+        {deleteOrder && (
+          <ConfirmationModal
+          successAction={handleDeleteOrder}
+          modalData={deleteOrder}
+          closeModal={closeModal}
+          successButtonName={`delete`}
+          message={`if you delete we cannot recover the data`}
+            title={`are you sure you want to delete?`}
+          ></ConfirmationModal>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
