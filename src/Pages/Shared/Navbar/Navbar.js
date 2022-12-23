cdf7aa17e30cee2b26589c95d2a6e625e5c4eb48
euhansarkar <euhansarkar@gmail.com>
2022-12-23 19:41:22 +0600

    user icon added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,8 +2,15 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 import { FaUserCircle } from "react-icons/fa";
-import avatar from "../../../assets/images/avatar/avatar.jpg"
-import logo from "../../../assets/images/logo/logo.png"
+import avatar from "../../../assets/images/avatar/avatar.jpg";
+import logo from "../../../assets/images/logo/logo.png";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
 
@@ -28,15 +35,6 @@ const Navbar = () => {
       <li className="font-bold">
         <Link to={`/dashboard`}>DashBoard</Link>
       </li>
-      {user?.email ? (
-        <li className="font-bold">
-          <button onClick={handleLogOut}>logout</button>
-        </li>
-      ) : (
-        <li className="font-bold">
-          <Link to={`/login`}>Login</Link>
-        </li>
-      )}
     </>
   );
 
@@ -75,15 +73,100 @@ const Navbar = () => {
         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
       <div className="navbar-end">
-          {
-            user?.email &&
-            <div className="avatar">
-            <div className="w-8 mr-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-              <img src={avatar} alt="" />
+        {user?.email ? (
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className={`flex items-center jusitfy-center`}>
+                <div className="avatar">
+                  <div className="w-8 mr-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src={avatar} alt="" />
+                  </div>
+                </div>
+              </Menu.Button>
             </div>
-          </div>
 
-          }
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="signup"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        signed in as <strong>{user.email && user.email}</strong>
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="/signup"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Account Settings
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="signup"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Support
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        onClick={handleLogOut}
+                        to="/signup"
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Sign out
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div className="py-1"></div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        ) : (
+          <Link to={`/login`}><button className="btn btn-primary btn-sm mr-10"  >log in</button></Link>
+        )}
       </div>
       <label
         tabIndex={2}
